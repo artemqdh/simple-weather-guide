e692754fe9d971ac23c55c07040ac52d3d52a824
TopAcademyBel <121047497+TopAcademy2022@users.noreply.github.com>
2025-03-27 21:09:43 +0300

    Moved project to Node.js. Closed #3 (#5)

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './script'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <p className='text-center'>data</p>
+);
