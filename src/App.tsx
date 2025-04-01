db8c46575bcb68236ac99999e9259982e021c54b
TopAcademyBel <121047497+TopAcademy2022@users.noreply.github.com>
2025-04-01 19:49:39 +0300

    Moved to typescript. Closed #10 (#11)

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <p className='text-center'>data</p>
+  );
+}
+
+export default App;
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './script'
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <p className='text-center'>data</p>
-);
